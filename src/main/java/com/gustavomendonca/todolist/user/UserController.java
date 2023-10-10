ae25ec4e2398a79614cf509a3d22688d59a6cbb2
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-10 14:28:23 -0300

    feat: users controller

---
+++ src/main/java/com/gustavomendonca/todolist/user/UserController.java
@@ -0,0 +1,21 @@
+package com.gustavomendonca.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * Body
+     * @param userModel
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+
+}
---
+++ src/main/java/com/gustavomendonca/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package com.gustavomendonca.todolist.user;
+
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+}
