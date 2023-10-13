440957289afd338267b83f0c91f6a6e01a933fe2
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-13 17:37:16 -0300

    feat: create tasks post endpoint

---
+++ src/main/java/com/gustavomendonca/todolist/task/TaskController.java
@@ -0,0 +1,22 @@
+package com.gustavomendonca.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository tasksRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel) {
+        var task = this.tasksRepository.save(taskModel);
+        return task;
+    }
+
+}
